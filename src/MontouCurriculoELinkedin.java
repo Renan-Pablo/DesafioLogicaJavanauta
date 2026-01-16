4075f3a14f8be6afc1cf9a0c760adfbd34d5d08d
Renan Pablo do Nascimento <kenanpablo123@gmail.com>
2026-01-16 13:30:24 -0300

    Update MontouCurriculoELinkedin.java

--- src/MontouCurriculoELinkedin.java
+++ src/MontouCurriculoELinkedin.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class MontouCurriculoELinkedin {
+
+//      Desafio 3
+//    Crie um programa que pergunte ao usuário se ele já montou um currículo e Linkedin
+//    organizado para a área da programação.
+//    Se sim → parabenizar
+//    Se não → motivar dizendo que currículo bem feito pode até compensar pouca
+//    experiência.
+
+    public static void main(String[] args) {
+        boolean curriculoMontado = true;
+        boolean linkedinOrganizado = false;
+
+        if (curriculoMontado && linkedinOrganizado) {
+            System.out.println("Parabéns pelo comprometimento!");
+        } else {
+            System.out.println("Um currículo bem feito e um Linkedin atualizado pode compensar pouca experiència!");
+        }
+    }
 }
